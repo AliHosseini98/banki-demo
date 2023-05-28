a0a6f083dbb45eb9f8c801877595df7ed572f425
seed Ai <alihny98@gmail.com>
2023-05-29 02:00:26 +0430

    add dto in controllerCustomer

--- banki/banki/banki/src/main/java/com/example/banki/modules/customer/controller/CustomerController.java
+++ banki/banki/banki/src/main/java/com/example/banki/modules/customer/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.banki.modules.customer.controller;
 
+import com.example.banki.modules.customer.CustomerDTO;
 import com.example.banki.modules.customer.model.Customer;
 import com.example.banki.modules.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,30 +16,33 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
     private CustomerService customerService;
+
     @Autowired
-    public CustomerController(CustomerService customerService){
-        this.customerService= customerService;}
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
 
     @PostMapping("/new")
-    public ResponseEntity<Customer> registerCustomer(@Valid @RequestBody Customer customer){
-        return  new ResponseEntity<>(customerService.save(customer), HttpStatus.CREATED);
+    public ResponseEntity<CustomerDTO> registerCustomer(@Valid @RequestBody Customer customer) {
+        return new ResponseEntity<>(customerService.save(customer), HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<Customer> updateCustomer (@RequestBody Customer customer){
-        return ResponseEntity.ok(customerService.update(customer));}
+    public ResponseEntity<CustomerDTO> updateCustomer(@RequestBody Customer customer) {
+        return new ResponseEntity<>(customerService.update(customer),HttpStatus.CREATED);
+    }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteById(@PathVariable int id){
+    public ResponseEntity<?> deleteById(@PathVariable int id) {
         customerService.delete(id);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/get/customers")
     @ResponseBody
-    public List<Customer> getAllCustomers(){
-      return   customerService.getAllCustomers();
+    public List<CustomerDTO> getAllCustomers() {
+        return customerService.getAllCustomers();
     }
 
 }
--- banki/banki/banki/src/main/java/com/example/banki/modules/customer/service/CustomerService.java
+++ banki/banki/banki/src/main/java/com/example/banki/modules/customer/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.example.banki.modules.customer.service;
 
+import com.example.banki.modules.customer.CustomerConvertor;
+import com.example.banki.modules.customer.CustomerDTO;
 import com.example.banki.modules.customer.model.Customer;
 import com.example.banki.modules.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
@@ -10,30 +12,41 @@ import java.util.List;
 public class CustomerService {
     private CustomerRepository customerRepository;
 
-    public CustomerService (CustomerRepository customerRepository){
-        this.customerRepository= customerRepository;
+    private final CustomerConvertor convertor;
+
+    public CustomerService(CustomerRepository customerRepository, CustomerConvertor convertor) {
+        this.customerRepository = customerRepository;
+        this.convertor = convertor;
     }
 
-    public Customer save (Customer customer){
-        return customerRepository.save(customer);
+    //check method for insert id for search
+    public CustomerDTO save(Customer customer) {
+        CustomerDTO dto = convertor.entityToDto(customer);
+        customerRepository.save(customer);
+        return dto;
     }
 
-    public Customer update(Customer customer){
-        Customer update_customer = customerRepository.findById(customer.getId()).orElseThrow(()-> new RuntimeException("The entered ID is not available"));
+    //check method
+    public CustomerDTO update(Customer customer) {
+        Customer update_customer = customerRepository.findById(customer.getId()).orElseThrow(() -> new RuntimeException("The entered ID is not available"));
         update_customer.setCard_id(customer.getCard_id());
         update_customer.setEmail(customer.getEmail());
         update_customer.setName(customer.getName());
         update_customer.setPhoneNumber(customer.getPhoneNumber());
         update_customer.setFamily(customer.getFamily());
-        return customerRepository.save(update_customer);
+        CustomerDTO dto = convertor.entityToDto(update_customer);
+        customerRepository.save(update_customer);
+
+        return dto;
     }
 
-    public void delete (int id){
+    public void delete(int id) {
         customerRepository.deleteById(id);
     }
 
 
-    public List<Customer> getAllCustomers(){
-       return customerRepository.findAll();
+    public List<CustomerDTO> getAllCustomers() {
+        List<CustomerDTO> customerDTOList = convertor.customerDTOList(customerRepository.findAll());
+        return customerDTOList;
     }
 }
