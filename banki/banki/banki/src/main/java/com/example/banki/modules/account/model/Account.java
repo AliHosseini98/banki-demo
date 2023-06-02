8a390ec436780a559615e51995a638cf11ada6fc
seed Ai <alihny98@gmail.com>
2023-06-02 08:02:09 +0430

    add table in tr class for account src and des

--- banki/banki/banki/src/main/java/com/example/banki/modules/account/model/Account.java
+++ banki/banki/banki/src/main/java/com/example/banki/modules/account/model/Account.java
@@ -43,4 +43,5 @@ public class Account {
         this.bankBranch = bankBranch;
     }
 
+
 }
--- banki/banki/banki/src/main/java/com/example/banki/modules/transaction/model/Transaction.java
+++ banki/banki/banki/src/main/java/com/example/banki/modules/transaction/model/Transaction.java
@@ -2,28 +2,34 @@ package com.example.banki.modules.transaction.model;
 
 import com.example.banki.modules.BaseEntity;
 import com.example.banki.modules.account.model.Account;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Table
 public class Transaction extends BaseEntity {
 
-    UUID transactionId=UUID.randomUUID();
+    UUID transactionId = UUID.randomUUID();
     @ManyToOne
+    @JoinTable(name = "transaction_account_source")
+    @JoinColumn(name = "account_id", referencedColumnName = "account_number")
     Account source;
 
     @ManyToOne
+    @JoinTable(name = "transaction_account_destination")
+    @JoinColumn(name = "account_id", referencedColumnName = "account_number")
     Account destination;
     double amount;
-    public Transaction() {
-    }
+
 
     public Transaction(Account source, Account destination, double amount) {
-
         this.source = source;
         this.destination = destination;
         this.amount = amount;
